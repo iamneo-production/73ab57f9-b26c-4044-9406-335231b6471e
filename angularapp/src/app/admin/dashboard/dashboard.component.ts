e6946fea102447fd41160a95dcd044f392d00294
SowmyaReddy-2918 <sowmyaramakrishna18@gmail.com>
2022-04-02 14:26:32 +0000

    commit

--- angularapp/src/app/admin/dashboard/dashboard.component.ts
+++ angularapp/src/app/admin/dashboard/dashboard.component.ts
@@ -1,38 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Furniture } from 'src/app/model/furniture';
+import { HttpClientService } from 'src/app/service/http-client.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit 
-{
+export class DashboardComponent implements OnInit {
+  [x: string]: any;
   dashboard: Array<Furniture>;
-  selectedBook: Furniture;
+  selectedFurniture: Furniture;
   action: string;
 
-  constructor() {}
-   
+  constructor(private httpClientService: HttpClientService,
+    private activedRoute: ActivatedRoute,
+    private router: Router) { }
 
-  ngOnInit(){
-    this.dashboard = new Array<Furniture>();
-
-    const f1 = new Furniture();
-    f1.id = 1;
-    f1.name = 'b1';
-    f1.company = 'a1';
-    f1.price = 5;
-
-    const f2 = new Furniture();
-    f2.id = 2;
-    f2.name = 'b2';
-    f2.company = 'a2';
-    f2.price = 15;
-
-    this.dashboard.push(f1);
-    this.dashboard.push(f2);
+  ngOnInit() {
+    this.refreshData();
   }
-    
 
+  refreshData() {
+    this.httpClientService.getdashboard().subscribe(
+      response => this.handleSuccessfulResponse(response)
+    );
+    this.activedRoute.queryParams.subscribe(
+      (params) => {
+        this.action = params['action'];
+        const id = params['id'];
+        if (id) {
+          this.selectedFurniture = this.dashboard.find(Furniture => {
+            return Furniture.id === +id;
+          });
+      }
+    );
+  }
+
+  handleSuccessfulResponse(response) {
+    this.dashboard = response;
+    this.dashboardRecieved = response;
+    for (const Furniture of this.dashboardRecieved) {
+    
+      const furniturewithRetrievedImageField = new Furniture();
+      furniturewithRetrievedImageField.id = Furniture.id;
+      furniturewithRetrievedImageField.name = Furniture.name;
+      furniturewithRetrievedImageField.retrievedImage = 'data:image/jpeg;base64,' + Furniture.picByte;
+      furniturewithRetrievedImageField.company = Furniture.company;
+      furniturewithRetrievedImageField.price = Furniture.price;
+      furniturewithRetrievedImageField.picByte=Furniture.picByte;
+      this.dashboard.push(furniturewithRetrievedImageField);
+    }
+  }
+
+  addFurniture() {
+    this.selectedFurniture = new Furniture();
+    this.router.navigate(['admin', 'dashboard'], { queryParams: { action: 'add' } });
+  }
 }
